9a954f7548b1fdcbceca611a47703992441927f4
ksssssw <rkdtjrdnjs1@gmail.com>
2025-07-27 13:09:29 +0900

    ktor 로깅 추가

--- spacex-module-architecture/core/network/build.gradle.kts
+++ spacex-module-architecture/core/network/build.gradle.kts
@@ -11,6 +11,7 @@ android {
 dependencies {
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.logging)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.client.serialization)
 
--- spacex-module-architecture/core/network/src/main/java/com/ksssssw/spacex/network/di/NetworkModule.kt
+++ spacex-module-architecture/core/network/src/main/java/com/ksssssw/spacex/network/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.ksssssw.spacex.network.di
 
+import android.util.Log
 import com.ksssssw.spacex.network.KtorSpaceXNetworkDataSource
 import com.ksssssw.spacex.network.SpaceXNetworkDataSource
 import io.ktor.client.HttpClient
@@ -7,6 +8,10 @@ import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
@@ -27,6 +32,15 @@ val networkModule = module {
 
             install(ContentNegotiation) {
                 json(get<Json>())
+            }
+
+            install(Logging) {
+                logger = object: Logger {
+                    override fun log(message: String) {
+                        Log.v("HTTP Client", message)
+                    }
+                }
+                level = LogLevel.BODY
             }
 
             install(HttpTimeout) {
